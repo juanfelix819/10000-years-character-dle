9fa88f9b91c65f45de62ae826b77f3c680657191
juanfelix819 <juanfelixlautama@gmail.com>
2026-09-13 13:35:27 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tenk-character-dle-v3";
+const CACHE = "tenk-character-dle-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
